14b168d9e5b8e4b8e4316341d06b8b2d4991519c
roberto <roberto24r24r@gmail.com>
2022-09-16 23:30:19 -0500

    Mostrar nombres de barras

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -58,7 +58,7 @@ export default {
                         enabled: false,
                     },
                     labels: {
-                        show: false
+                        show: true
                     }, 
 
                     
